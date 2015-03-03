acdc346be919d2ae0bd796017097f7f216a80cbd
LeeYeeze <yizeli@Yizes-MacBook-Pro.local>
2015-03-02 22:27:32 -0500

    MinHeap

--- hidash.js
+++ hidash.js
@@ -40,11 +40,67 @@ function Queue() {
 }
 
 
-function minHeap() {
+function MinHeap() {
+    this.heap = [];
+    this.size = 0;
+}
+
+MinHeap.prototype.sink = function(position){
+    var childIndex = position*2;
+    var rearrange = this.heap[position];
+    while (childIndex <= this.size) {
+        if(childIndex<this.size && this.heap[childIndex+1]<this.heap[childIndex])
+            childIndex++;
+        if (this.heap[childIndex]>=rearrange)
+            break;
+        this.heap[position] = this.heap[childIndex];
+        position = childIndex;
+        childIndex = position*2;
+    }
+    this.heap[position] = rearrange;
+}
+
+MinHeap.prototype.removeMin = function() {
+    var rootVal = this.heap[1];
+    this.heap[1] = this.heap[this.size--];
+    this.sink(1);
+    return rootVal;
+}
+
+MinHeap.prototype.insert = function(x) {
+    this.heap[++this.size] = x;
+    this.swim(this.size);
+}
+
+MinHeap.prototype.swim = function(position) {
+    var parentIndex = Math.floor(position/2);
+    var insertVal = this.heap[position];
+    while (parentIndex>0 && insertVal<this.heap[parentIndex]) {
+        this.heap[position] = this.heap[parentIndex];
+        position = parentIndex;
+        parentIndex = Math.floor(position/2);
+    }
+    heap[position] = insertVal;
+}
+
+function MaxHeap() {
+    this.heap = [];
+    this.size = 0;
+}
+
+MaxHeap.prototype.sink = function(position) {
 
 }
 
-function maxHeap() {
+MaxHeap.prototype.removeMax = function() {
+
+}
+
+MaxHeap.prototype.swim = function(position) {
+
+}
+
+MaxHeap.prototype.insert = function(x) {
 
 }
 
