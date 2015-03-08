3137d89c1c71b2e9cfb7a60705de453ee7971f49
LeeYeeze <yizeli@Yizes-MacBook-Pro.local>
2015-03-08 19:59:47 -0400

    radix sort and counting sort

--- SuffixArray.js
+++ SuffixArray.js
@@ -15,17 +15,72 @@ function quick3WaySort(arr) {
 
 }
 
-function radixSort(arr) {
+function radixPass() {
 
 }
 
-function constructSuffixArray(s) {
-    var T = [];
-    for (var i = 0; i*3 < s.length; i++) {
-        if (i+1< s.length)
-            T.push(s.substring(i+1, i+4));
+function getMax(arr) {
+    var res = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > res)
+            res = arr[i];
     }
-    
+    return res;
+}
+
+function countSort(arr, exp) {
+    var res = [];
+    var count = [];
+    for (var i = 0; i < 10; i++) {
+        count[i] = 0;
+    }
+    for (var i =0; i < arr.length; i++) {
+        count[Math.floor(arr[i]/exp)%10]++
+    }
+
+    for (var i = 1; i < 10; i++) {
+        count[i]+=count[i-1];
+    }
+
+    for (var i = arr.length -1; i >= 0; i--) {
+        res[count[Math.floor(arr[i]/exp)%10]-1] = arr[i];
+        count[Math.floor(arr[i]/exp)%10]--;
+    }
+
+    for (var i = 0; i<arr.length; i++) {
+        arr[i] = res[i];
+    }
+
+}
+
+function radixSort(arr) {
+    var max = getMax(arr);
+    for (var exp = 1; Math.floor(max/exp)>0; exp *=10) {
+        countSort(arr, exp);
+    }
+    return arr;
+}
+
+
+
+function toCharacterCodeArray(s) {
+    var cc = [];
+    for (var i = 0; i < s.length; i++) {
+        cc[i] = s.charCodeAt(i);
+    }
+}
+
+function constructSuffixArray(s) {
+    var charCodeArray = toCharacterCodeArray(s);
+    var len = s.length;
+    charCodeArray[len] = 0;
+    charCodeArray[len+1] = 0;
+    charCodeArray[len+2] = 0;
+    var n1 = Math.floor((len+2)/3);
+    var n2 = Math.floor((len+1)/3);
+    var n0 = Math.floor(len/3);
+
+
 
 }
 
@@ -64,3 +119,5 @@ SuffixArray.prototype.lcp = function(i) {
 SuffixArray.prototype.index = function(i) {
     return this.suffixes.length - this.suffixes[i].length;
 };
+
+console.log(radixSort([4,3,5,6,7]));
