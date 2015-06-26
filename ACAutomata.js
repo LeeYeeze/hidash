1f9ac06d1cb7c898bbf669db6e6cab94afb7ae1e
LeeYeeze <yl2376@cornell.edu>
2015-06-26 19:13:39 +0800

    AC

--- ACAutomata.js
+++ ACAutomata.js
@@ -1,21 +1,126 @@
 /**
  * Created by yizeli on 6/4/15.
  */
+
 function ACNode() {
+    this.code = null;
     this.children = [];
     this.parent = null;
+    this.pre = null;
+    this.isWord = false;
+    this.level = 0;
+    this.count = 0;
 }
+
+ACNode.prototype.setPre = function(root) {
+
+    if (this.parent === null) {
+        return;
+    } else {
+        var candidate  = this.parent.pre;
+        while (candidate !== null) {
+            if (typeof candidate.children[this.code] !== "undefined") {
+                this.pre = candidate.children[this.code];
+                break;
+            } else {
+                candidate = candidate.pre;
+            }
+        }
+        if (this.pre === null) {
+            this.pre = root;
+        }
+    }
+};
+
+ACNode.prototype.findPre = function() {
+    return this.pre;
+};
+
+
 function ACAutomata(arrayOfStrings) {
     this.root = new ACNode();
     this.init(arrayOfStrings);
 }
 
 ACAutomata.prototype.init = function(arrayOfStrings) {
+    var current;
     for (var i = 0; i < arrayOfStrings.length; i++) {
+        current = this.root;
+        for (var j = 0; j < arrayOfStrings[i].length; j++) {
+            if (typeof current.children[arrayOfStrings[i].charCodeAt(j)] === "undefined") {
+                current.children[arrayOfStrings[i].charCodeAt(j)] = new ACNode();
+                current.children[arrayOfStrings[i].charCodeAt(j)].parent = current;
+                current.children[arrayOfStrings[i].charCodeAt(j)].code = arrayOfStrings[i].charCodeAt(j);
+                current.children[arrayOfStrings[i].charCodeAt(j)].level = current.level + 1;
+                //current.children[arrayOfStrings[i].charCodeAt(j)].count = current.count;
+            }
+            current = current.children[arrayOfStrings[i].charCodeAt(j)];
+            if (j === arrayOfStrings[i].length - 1) {
+                current.isWord = true;
+                current.count = 1;
+            }
+        }
+    }
+    var stack = [this.root];
+    var newStack = [];
+    var level = 0;
+    while (stack.length > 0) {
+        var node = stack.pop();
 
+        if (level == 1) {
+            node.pre = this.root;
+        } else if (level > 1) {
+            node.setPre(this.root);
+        }
+
+        if (node !== this.root) {
+            node.count  = node.pre.count + (node.isWord? 1: 0);
+        }
+
+        for (var i = 0 ; i < node.children.length; i++) {
+            if(typeof node.children[i] !== "undefined") {
+                newStack.push(node.children[i]);
+            }
+        }
+
+        if (stack.length === 0) {
+            level++;
+            var temp = stack;
+            stack = newStack;
+            newStack = temp;
+        }
     }
 };
 
-ACAutomata.prototype.search = function() {
+ACAutomata.prototype.searchWithDup = function(text) {
+    var current = this.root;
+    var res = 0;
+    for (var i = 0; i < text.length; i++) {
+        while (current !== this.root && typeof current.children[text.charCodeAt(i)] === "undefined") {
+            current = current.pre;
+        }
+        current = current.children[text.charCodeAt(i)];
+        if (typeof current === "undefined") {
+            current = this.root;
+        }
+        /*
+        var temp = current;
+        while (temp !== this.root) {
+            res += temp.isWord? 1: 0;
+            temp = temp.pre;
+        }
+        */
 
-}
+        res += current.count;
+
+    }
+    return res;
+};
+
+ACAutomata.prototype.searchWithoutDup = function() {
+
+};
+
+var ac = new ACAutomata(["apple","pear","insomnia","app","p"]);
+console.log(ac.root);
+console.log(ac.searchWithDup("apppear"));
--- Geometry.js
+++ Geometry.js
@@ -1,3 +1,61 @@
 /**
  * Created by yizeli on 6/24/15.
  */
+function Polygon(pointArray) {
+
+}
+
+function Point2D(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+}
+
+function Line2D(end1, end2) {
+    this.end1 = end1;
+    this.end2 = end2;
+}
+
+function orientation(p,q,r) {
+    var val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (val == 0) {
+        return 0;
+    }
+    return (val > 0) ? 1:2;
+}
+
+function onSegment(p,q,r) {
+    if (r.x <= Math.max(p.x, q.x) && r.x >= Math.min(p.x, q.x) && r.y <= Math.max(p.y, q.y) && r.y >= Math.min(p.y, q.y)) {
+        return true;
+    }
+    return false;
+}
+
+function intersect(p1, q1, p2, q2) {
+    var o1 = orientation(p1,q1,p2);
+    var o2 = orientation(p1,q1,q2);
+    var o3 = orientation(p2,q2,p2);
+    var o4 = orientation(p2,q2,p1);
+    if (o1 !== o2 && o3 !== o4) {
+        return true;
+    } else if (o1 === 0 && o2 === 0 && o3 === 0 && o4 === 0) {
+        if (onSegment(p1,q1,p2) || onSegment(p1,q1,q2)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+function lineIntersect(line1, line2) {
+    return intersect(line1.end1, line1.end2, line2.end1, line2.end2);
+};
+
+var p1 = new Point2D(1,1),
+    p2 = new Point2D(1.5,1.5),
+    p3 = new Point2D(2,2),
+    p4 = new Point2D(4,4);
+
+console.log(intersect(p1,p2,p3,p4));
+
